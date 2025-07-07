1b5d742fa26daf3f674f68db6d660954e05c82b9
NandaScott <nanda1123@gmail.com>
2025-07-07 14:05:53 -0400

    Removed state from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 import { Tab, TabGroup, TabList, TabPanels } from '@headlessui/react';
 import clsx from 'clsx';
 import RulesPanel from './panels/RulesPanel';
@@ -6,15 +6,12 @@ import StratagemsPanel from './panels/StratagemsPanel';
 import DatasheetsPanel from './panels/DatasheetsPanel';
 
 function App() {
-  const [points, setPoints] = useState(2000);
-  console.log(setPoints);
-
   return (
     <div className='p-8 flex flex-col gap-6'>
       <h2 className='text-3xl font-bold'>Miracle Points</h2>
       <div className='flex gap-4 items-baseline'>
         <div className='text-3xl font-semibold'>Army of Faith</div>
-        <span className='text-xl italic font-semibold'>{points} / 2000</span>
+        <span className='text-xl italic font-semibold'>2000 / 2000</span>
       </div>
       <TabGroup className='w-full'>
         <TabList className='flex mb-4'>
